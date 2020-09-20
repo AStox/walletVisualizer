3dff7db618cc3478ce3e3f6e7cf9d72f2efac232
Adam Stockermans <adamstox@gmail.com>
2020-09-20 18:48:19 -0400

    Add flask backend

---
+++ server/api.py
@@ -0,0 +1,28 @@
+import time
+import requests
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+my_account = os.environ.get("MY_ACC")
+etherscan_api_key = os.environ.get("ETHERSCAN_API_KEY")
+
+
+@app.route("/")
+def main():
+    return "hi"
+
+
+@app.route("/<wallet>")
+def get_transactions(wallet):
+    response = requests.get(
+        f"https://api.etherscan.io/api?module=account&action=txlist&address={wallet}&startblock=0&endblock=99999999&sort=asc&apikey={etherscan_api_key}"
+    )
+    transactions = response.json()["result"]
+    return {"transactions": transactions}
+
+
+@app.route("/time")
+def get_current_time():
+    return {"time": time.time()}
