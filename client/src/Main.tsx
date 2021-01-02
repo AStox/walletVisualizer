82b44d893cb30562ab86bcba0722a40ced6d4d7c
Adam Stockermans <adamstox@gmail.com>
2021-01-01 20:01:20 -0400

    add frontend handling for new task

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import AddressInput from "./AddressInput";
 import WalletGraph from "./WalletGraph";
@@ -21,94 +21,99 @@ const Main = () => {
       }
     | undefined
   >(undefined);
-  // const [allTokens, setAllTokens] = useState<string[] | undefined>(undefined);
   const [transaction, setTransaction] = useState({});
   const [address, setAddress] = useState("");
   const [showUSD, setShowUSD] = useState(true);
+  const [taskPollingUrl, setTaskPollingUrl] = useState(null);
 
-  // useEffect(() => {
-  //   // if (targetRef.current) {
-  //   //   setDimensions({
-  //   //     width: targetRef.current.offsetWidth,
-  //   //     height: targetRef.current.offsetHeight,
-  //   //   });
-  //   // }
-  //   const data = {
-  //     address: "0x225ef95fa90f4F7938a5b34234d14768cb4263dd".toLowerCase(),
-  //   };
-  //   useMemo(() => onSubmit(data), [data]), [];
-  // });
+  useEffect(() => {
+    if (taskPollingUrl) {
+      fetchTaskStatus();
+    }
+  }, [taskPollingUrl]);
+
+  const fetchTaskStatus = () => {
+    fetch(taskPollingUrl)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res.state != "PENDING" && res.state != "PROGRESS") {
+          console.log(res);
+        } else {
+          fetchTaskStatus();
+        }
+      });
+  };
+
+  const formatRawData = (data) => {
+    const mergeArrays = (
+      transactions1: Transaction[],
+      transactions2: Transaction[]
+    ) => {
+      let dupesArray: number[] = [];
+      let returnArray: Transaction[] = [];
+      transactions1.pop();
+      forEach(transactions1, (tx1) => {
+        let dupe = false;
+        forEach(transactions2, (tx2: Transaction) => {
+          if (tx1.timeStamp === tx2.timeStamp) {
+            dupesArray.push(transactions2.indexOf(tx2));
+            dupe = true;
+            // returnArray.push({
+            //   ...tx1,
+            //   ...tx2,
+            //   balances: { ...tx1.balances, ...tx2.balances },
+            //   balancesUSD: { ...tx1.balancesUSD, ...tx2.balancesUSD },
+            // });
+          }
+        });
+        if (!dupe) {
+          returnArray.push(tx1);
+        }
+      });
+      const lastTx = returnArray[returnArray.length - 1];
+      // returnArray.pop();
+      forEach(transactions2, (tx) => {
+        if (!includes(dupesArray, transactions2.indexOf(tx))) {
+          returnArray.push({
+            ...lastTx,
+            ...tx,
+            balances: { ...lastTx.balances, ...tx.balances },
+            balancesUSD: { ...lastTx.balancesUSD, ...tx.balancesUSD },
+          });
+        }
+      });
+      return returnArray;
+    };
+
+    if (addressData) {
+      const transactions = mergeArrays(
+        addressData.transactions,
+        data.transactions
+      );
+
+      console.log("old: ", addressData.transactions);
+      console.log("new: ", transactions);
+
+      setAddressData({
+        last_block_number: addressData?.last_block_number,
+        transactions: transactions,
+        all_tokens: [...addressData?.all_tokens, ...data.all_tokens],
+      });
+    } else {
+      setAddressData(data);
+    }
+    setAddress(address);
+  };
 
   const onSubmit = (data: any) => {
     const address = data.address.toLowerCase();
     const blockNumber = addressData?.last_block_number || 0;
     const url = `/api/wallet/${address}?blockNumber=${blockNumber}`;
-    // url.search = new URLSearchParams(params).toString();
-    // console.log(url.toString());
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        const mergeArrays = (
-          transactions1: Transaction[],
-          transactions2: Transaction[]
-        ) => {
-          let dupesArray: number[] = [];
-          let returnArray: Transaction[] = [];
-          transactions1.pop();
-          forEach(transactions1, (tx1) => {
-            let dupe = false;
-            forEach(transactions2, (tx2: Transaction) => {
-              if (tx1.timeStamp === tx2.timeStamp) {
-                dupesArray.push(transactions2.indexOf(tx2));
-                dupe = true;
-                // returnArray.push({
-                //   ...tx1,
-                //   ...tx2,
-                //   balances: { ...tx1.balances, ...tx2.balances },
-                //   balancesUSD: { ...tx1.balancesUSD, ...tx2.balancesUSD },
-                // });
-              }
-            });
-            if (!dupe) {
-              returnArray.push(tx1);
-            }
-          });
-          const lastTx = returnArray[returnArray.length - 1];
-          // returnArray.pop();
-          forEach(transactions2, (tx) => {
-            if (!includes(dupesArray, transactions2.indexOf(tx))) {
-              returnArray.push({
-                ...lastTx,
-                ...tx,
-                balances: { ...lastTx.balances, ...tx.balances },
-                balancesUSD: { ...lastTx.balancesUSD, ...tx.balancesUSD },
-              });
-            }
-          });
-          return returnArray;
-        };
-
-        if (addressData) {
-          const transactions = mergeArrays(
-            addressData.transactions,
-            data.transactions
-          );
-
-          console.log("old: ", addressData.transactions);
-          console.log("new: ", transactions);
-
-          setAddressData({
-            last_block_number: addressData?.last_block_number,
-            transactions: transactions,
-            all_tokens: [...addressData?.all_tokens, ...data.all_tokens],
-          });
-        } else {
-          setAddressData(data);
-        }
-        setAddress(address);
-      });
-    // console.log(addressData);
+    fetch(url).then((res) => setTaskPollingUrl(res.headers.get("location")));
+    // .then((res) => res.json())
+    // .then((data) => formatRawData(data));
   };
 
   return (
